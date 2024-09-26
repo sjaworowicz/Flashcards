9b20333289f604384b6f9a6863ab07d7fee3b4fb
Silas Jaworowicz <125sjaworowicz@frhsd.com>
2024-09-26 08:16:25 -0400

    started js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const flashcards = document.getElementsByClassName("flashcards")[0];
+const createBox = document.getElementsByClassName("create-box")[0];
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
